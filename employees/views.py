9655c27b0c06a7f12a4dbeb868b14edefdf6d04e
Girish Gopaul <gopaul0510@gmail.com>
2021-08-26 20:58:15 +0400

    Added update and delete to employees

--- employees/views.py
+++ employees/views.py
@@ -1,5 +1,6 @@
 from django.http.response import HttpResponse, JsonResponse
 from django.views.decorators.http import require_http_methods
+from django.forms.models import model_to_dict
 
 from rest_api.json import AuthorizedJsonRequest
 from users.auth import authenticate_request
@@ -10,24 +11,48 @@ from .models import Employee
 @authenticate_request
 def index(request: AuthorizedJsonRequest) -> HttpResponse:
     if request.method == 'GET':
-        return list_employees(request)
+        return __list_employees(request)
     else:
-        return create_employee(request)
+        return __create_employee(request)
 
 
-def list_employees(request: AuthorizedJsonRequest) -> HttpResponse:
+@require_http_methods(['PUT', 'DELETE'])
+@authenticate_request
+def resource(request: AuthorizedJsonRequest, pk: int) -> HttpResponse:
+    if request.method == 'PUT':
+        return __update(request, pk)
+    else:
+        return __delete(request, pk)
+
+
+def __update(request: AuthorizedJsonRequest, pk: int) -> HttpResponse:
+    employee_params = request.json.get('employee', {})
+    employee_queryset = Employee.objects.filter(pk=pk)
+    employee_queryset.update(**employee_params)
+    return JsonResponse({
+        'data': model_to_dict(employee_queryset[0])
+    })
+
+
+def __delete(request: AuthorizedJsonRequest, pk: int) -> HttpResponse:
+    Employee.objects.get(pk=pk).delete()
+    return HttpResponse(status=204)
+
+
+def __list_employees(request: AuthorizedJsonRequest) -> HttpResponse:
     all_employees = list(Employee.objects.values())
     return JsonResponse({
         'data': all_employees,
     })
 
 
-def create_employee(request: AuthorizedJsonRequest) -> HttpResponse:
-    employee = request.json.get('employee', {})
-    Employee(**employee).save()
+def __create_employee(request: AuthorizedJsonRequest) -> HttpResponse:
+    employee_params = request.json.get('employee', {})
+    employee = Employee(**employee_params)
+    employee.save()
     return JsonResponse(
         data={
-            'data': employee
+            'data': model_to_dict(employee)
         },
         status=201,
     )
